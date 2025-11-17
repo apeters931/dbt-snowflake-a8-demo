4ac3f180ebdddb8935b5c6abac6f5c10e08a903c
Alec Peters <alecpeters610@gmail.com>
2025-11-17 15:53:09 -0600

    update dbt project id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "dbt_project_id" {
   description = "The ID of the dbt Cloud project where all resources should be deployed."
   type        = number
 }
-
-variable "git_repository_id" {
-  description = "The unique numeric ID of the Git repository (e.g., GitHub repo ID)."
-  type        = number
-}
